08e47b0e8d89eff7d53074f23007c39aa875ac31
Pozitrone <jirirkas.skola@gmail.com>
2020-08-13 09:16:44 +0200

    Added tests

---
+++ Runtime/Tests/MonoBehaviourTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TheAshenWolf;
+using UnityEngine;
+
+namespace Plugins.com.TheAshenWolf.Lib.Runtime.Tests
+{
+    public class MonoBehaviourTest : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+
+        public void NoiseTest()
+        {
+            Noise2D noise = new Noise2D(10, 10);
+            Debug.Log(noise.Seed);
+            foreach (double line in noise.Noise)
+            {
+                Debug.Log(line);
+            }
+        }
+    }
+}
