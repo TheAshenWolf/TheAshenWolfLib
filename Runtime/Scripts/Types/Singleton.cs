587c99bda10fdfecf4b7cfc1e223222f26ba069a
Pozitrone <jirirkas.skola@gmail.com>
2020-11-26 13:10:43 +0100

    Added singleton interface

---
+++ Runtime/Scripts/Types/Singleton.cs
@@ -0,0 +1,8 @@
+﻿namespace TheAshenWolf.Types
+{
+    public interface Singleton
+    {
+        // Interface used to just mark the classes as singleton.
+        // This way, one can clearly say whether the class is singleton or not.
+    }
+}
