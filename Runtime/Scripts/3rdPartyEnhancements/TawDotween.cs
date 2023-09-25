0414d8a42684a2663dbccc20910e919977346cfd
TheAshenWolf <jirirkas.skola@gmail.com>
2023-09-25 11:31:51 +0200

    Added DOFlash method

--- Runtime/Scripts/3rdPartyEnhancements/TawDotween.cs
+++ Runtime/Scripts/3rdPartyEnhancements/TawDotween.cs
@@ -8,6 +8,9 @@ using UnityEngine;
 
 namespace Plugins.TheAshenWolfLib.Runtime.Scripts._3rdPartyEnhancements
 {
+  /// <summary>
+  /// Extension methods for DOTween
+  /// </summary>
   public static class TawDotween
   {
     /// <summary>
@@ -32,6 +35,20 @@ namespace Plugins.TheAshenWolfLib.Runtime.Scripts._3rdPartyEnhancements
       TweenerCore<float, float, FloatOptions> t = DOTween.To(() => target.orthographicSize,
         x => target.orthographicSize = x, endValue, duration);
       return t;
+    }
+
+    /// <summary>
+    /// Flashes the text color of a TextMeshProUGUI into a different color and returns it to the original color
+    /// </summary>
+    /// <param name="target">Targeted TextMeshPro</param>
+    /// <param name="endValue">Color to flash into</param>
+    /// <param name="duration">Speed of the flash</param>
+    /// <param name="repeat">How many times to repeat the flashing</param>
+    public static TweenerCore<Color, Color, ColorOptions> DOFlash(this TextMeshProUGUI target, Color endValue,
+      float duration, int repeat = 1)
+    {
+      TweenerCore<Color, Color, ColorOptions> tween = target.DOColor(endValue, duration / 2f).SetLoops(repeat * 2, LoopType.Yoyo);
+      return tween;
     }
   }
 }
